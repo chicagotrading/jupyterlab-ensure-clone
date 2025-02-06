8d03eff0e54a87dcb899c9a0d5b25dd54fc30914
Joshua Bronson <joshua.bronson@chicagotrading.com>
2025-02-05 21:02:09 -0500

    handle more edge cases

--- jupyterlab_ensure_clone/handlers.py
+++ jupyterlab_ensure_clone/handlers.py
@@ -14,6 +14,10 @@ logger = logging.getLogger("jupyterlab_ensure_clone")
 logger.setLevel(logging.DEBUG)
 
 
+def git(*args):
+    subprocess.check_call(("git", *args), env={**os.environ, "GIT_TERMINAL_PROMPT": "0"})
+
+
 class RouteHandler(APIHandler):
     @tornado.web.authenticated
     def post(self):
@@ -24,35 +28,53 @@ class RouteHandler(APIHandler):
         parsedUrl = urlparse(repoUrl)
         if not all((parsedUrl.scheme, parsedUrl.netloc, parsedUrl.path)):
             raise tornado.web.HTTPError(400, "invalid repoUrl")
-        targetDir = data.get("targetDir", parsedUrl.path.rsplit("/", 1)[-1]).removesuffix(".git")
-        targetDir = Path(targetDir).expanduser()
-        if targetDir.is_dir():
-            logger.debug("targetDir %s exists, assuming repo already cloned there", targetDir)
-            updateScript = data.get("updateScript")
-            if updateScript:
-                try:
-                    subprocess.check_call((updateScript,), env={**os.environ, "GIT_TERMINAL_PROMPT": "0"})
-                except subprocess.CalledProcessError:
-                    logger.debug("Failed to update (expected in the pre-dialog check in the needCredentials case), see output above")
-                    raise tornado.web.HTTPError(400, reason="Failed to update, maybe due to bad credentials") from None
-            self.set_status(204)
-            self.finish()
-            return
         username = data.get("username")
         password = data.get("password")
+        repoUrlOrig = repoUrl
         if username or password:
             username = quote(username, safe='')
             password = quote(password, safe='')
             repoUrl = f"https://{username}:{password}@{parsedUrl.netloc}{parsedUrl.path}"
+        targetDir = data.get("targetDir", parsedUrl.path.rsplit("/", 1)[-1]).removesuffix(".git")
+        targetDir = Path(targetDir).expanduser()
+        if targetDir.is_dir():
+            targetDir = str(targetDir)
+            logger.info("targetDir %s exists, attempting git fetch...", targetDir)
+            try:
+                git("-C", targetDir, "fetch", "-q")
+            except subprocess.CalledProcessError:
+                if repoUrlOrig != repoUrl:
+                    logger.info("fetch failed, trying again with provided credentials")
+                    try:
+                        git("-C", targetDir, "remote", "set-url", "origin", repoUrl)
+                        git("-C", targetDir, "fetch", "-q")
+                    except subprocess.CalledProcessError:
+                        pass  # fall through to failure with 400 error below
+                    else:
+                        logger.info("fetch succeeded, removing credentials from origin URL")
+                        git("-C", targetDir, "remote", "set-url", "origin", repoUrlOrig)
+                        self.set_status(204)
+                        self.finish()
+                        return
+                logger.info("fetch failed (expected in the pre-dialog check in the needCredentials case), see output above")
+                raise tornado.web.HTTPError(400, reason="Failed to update, maybe due to bad credentials") from None
+            self.set_status(204)
+            self.finish()
+            return
         targetDir = str(targetDir)
+        logger.info("cloning into %s...", targetDir)
         try:
-            subprocess.check_call(("git", "clone", repoUrl, targetDir), env={**os.environ, "GIT_TERMINAL_PROMPT": "0"})
+            git("clone", "-q", repoUrl, targetDir)
         except subprocess.CalledProcessError:
-            logger.debug("Failed to clone repo (expected in the pre-dialog check in the needCredentials case), see output above")
+            logger.info("clone failed (expected in the pre-dialog check in the needCredentials case), see output above")
             raise tornado.web.HTTPError(400, reason="Failed to clone, maybe due to bad credentials") from None
-        logger.debug("cloned repo into %r", targetDir)
-        self.set_status(204)
-        self.finish()
+        else:
+            logger.info("cloned into %r", targetDir)
+            if repoUrlOrig != repoUrl:
+                logger.info("removing credentials from origin URL")
+                git("-C", targetDir, "remote", "set-url", "origin", repoUrlOrig)
+            self.set_status(204)
+            self.finish()
 
 
 def setup_handlers(web_app):
